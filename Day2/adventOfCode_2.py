aa2ec7a64f2b9a159308790a3f2bbecf913e543d
estr4ng7d <baviskaramey@gmail.com>
2019-06-21 13:51:57 -0700

    Day2 Part 1

--- Day2/adventOfCode_2.py
+++ Day2/adventOfCode_2.py
@@ -1,6 +1,19 @@
+from collections import Counter
 
-ip = 10
-idx=0
-while idx < 100:
-	idx = (idx+1) % ip
-	print(idx)
+file = open('adventOfCode_2.txt','r')
+twos_count=0
+threes_count=0
+
+for line in file:
+	twos_count_flag = False
+	threes_count_flag = False
+	count = Counter(line)
+	for td in count.items():
+		if td[1] == 2 and twos_count_flag == False:
+			twos_count+=1
+			twos_count_flag = True
+		if td[1] == 3 and threes_count_flag == False:
+			threes_count_flag = True
+			threes_count+=1
+print(twos_count,threes_count)
+print(twos_count*threes_count)
